2280db9318a46e179e479b29f0d7cf22a75447f0
June-Senpai <keshav01juneja@gmail.com>
2024-08-31 21:47:57 +0530

    style: home page for small screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import Weather from "./components/Weather";
 
 export default function Home() {
   return (
-    <main className="flex h-screen justify-center items-center flex-col gap-4">
+    <main className="flex h-screen justify-start items-center md:justify-center flex-col gap-4 p-8">
       <Image
         src={"./logo-white-text.svg"}
         height={111}
         width={330}
         alt="Logo"
         priority
-        className="bg-[#1b1b1d] p-2 rounded-lg"
+        className="bg-[#1b1b1d] p-2 rounded-lg "
       />
       <Weather />
     </main>
